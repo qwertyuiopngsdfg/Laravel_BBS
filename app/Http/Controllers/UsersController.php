47dc60c1fcf58f019ecfa7038dd38f22c14c43ee
qwertyuiopngsdfg <pngsdfg@outlook.jp>
2018-04-08 19:52:49 +0000

    save post

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use App\Tag;
+use Illuminate\Support\Facades\Hash;
+
+class UsersController extends Controller
+{
+    public function save(Request $request) {
+        $user = new User();
+        if ($request->name) $user->name = $request->name;
+        if ($request->name) $user->title = $request->title;
+        $user->skype = $request->skype;
+        $user->sex = $request->sex;
+        $user->body = $request->body;
+        $user->password = Hash::make($request->password);
+        $user->save();
+        //タグが挿入されている場合の処理　されていなければリダイレクト。
+        if (!$request->tags) return redirect('/');
+        if (is_array($request->tags)) {
+            foreach ($request->tags as $tag) {
+                $tag_db = new Tag();
+                $tag_db->entry_id = $user->id;
+                $tag_db->tag = $tag;
+                $tag_db->save();
+            }   return redirect('/');
+        } else { //タグの数が１つの場合の処理
+            $tag_db = new Tag();
+            $tag_db->entry_id = $user->id;
+            $tag_db->tag = $request->tags;
+            $tag_db->save();
+            return redirect('/');
+        }
+    }
+
+}
