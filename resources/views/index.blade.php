3142533e08156cdae5d5f1459fe024cf258dce72
qwertyuiopngsdfg <pngsdfg@outlook.jp>
2018-04-08 18:31:14 +0000

    add index.php

---
+++ resources/views/index.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="ja">
+	<head>
+		<meta charset="utf-8">
+		<title>掲示板</title>
+	<link rel="stylesheet" href="/css/styles.css">
+	</head>
+	<body>
+		<h1><a href="{{ url('/') }}">Skype BBS</a></h1>
+		<p>アクティブユーザー： 10256人</p>
+        <div class="menu">
+            <ul>
+                <li><a href="">投稿</a></li>
+                <li><a href="">検索</a></li>
+            </ul>
+        </div>
+        <p>記事一覧</p>
+        <dl>
+            <dt>
+                <p>name:名無しさん</p>
+                <p>skype:nanashi774</p>
+            </dt>
+            <dd>
+                <ul>
+                    <li>Tag1</li>
+                    <li>Tag2</li>
+                </ul>
+                <p>title: hello</p>
+                <p>body: hi! im qwertyuiopngsdfg!!</p>
+            </dd>
+        </dl>
+</html> 
